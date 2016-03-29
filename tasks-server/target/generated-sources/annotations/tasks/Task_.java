a37bdae8fdc9c00329c193f533970ce893f974ac
VinhLM <vinh.lu-minh@edu.esiee.fr>
2016-03-29 22:19:26 +0200

    Sources test pour jenkins

---
+++ tasks-server/target/generated-sources/annotations/tasks/Task_.java
@@ -0,0 +1,15 @@
+package tasks;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-20T21:18:02")
+@StaticMetamodel(Task.class)
+public class Task_ { 
+
+    public static volatile SingularAttribute<Task, Integer> id;
+    public static volatile SingularAttribute<Task, String> title;
+    public static volatile SingularAttribute<Task, String> description;
+
+}
